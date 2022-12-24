f8e8ece5e59866b1239e4561d7d512bc7c316d74
VladimirSherban <Vovan_Sh_94@mail.ru>
2022-12-24 22:57:18 +0300

    Course task done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1, 2, 3, 4, 5, 6};
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+            if (i != 0){
+                System.out.print(", ");
+            }
+        }
     }
 }
